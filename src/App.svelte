0d43e01ee40e31b1317092f658c3e117b918532b
Sal Carrera <carrera.328@gmail.com>
2023-02-11 02:10:03 -0500

    initial-commit

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+
+  let count = 0;
+  let string = `You have clicked ${count}`
+  
+  function handleClick(event) {
+    console.log(event.target.dataset.id);
+  }
+
+  function handleChange(event) {
+    console.log('search event', event);
+  }
+</script>
+
+<main>
+  <h2>{string}</h2>
+  <button data-id="btn" on:click={handleClick}>Fuck Camille Jackson</button>
+  <input on:keydown={handleChange} type='search'/>
+  <h1>Hello World</h1>
+</main>
+
